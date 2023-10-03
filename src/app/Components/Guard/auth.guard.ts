690d76ff3fe7db32d5e90b8064720b51a8bb5a9a
saurabhgit <144985369+SaurabhGajbhiyeSahaligram@users.noreply.github.com>
2023-10-03 14:03:41 +0530

    sau

--- src/app/Components/Guard/auth.guard.ts
+++ src/app/Components/Guard/auth.guard.ts
@@ -1,20 +1,21 @@
-import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
-import { AuthService } from "../Services/auth.service";
-import { Injectable } from "@angular/core";
- 
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { AuthService } from '../Services/auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard {
+
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authService.isLoggedIn()) {
-      
       return true;
-    } else {
-   
+    } else { 
       this.router.navigate(['/']);
       return false;
     }
